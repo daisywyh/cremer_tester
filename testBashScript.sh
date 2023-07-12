8467a13f61124b498b820ce9c0592ae226e95b7c
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-12 23:50:23 +0000

    fixed typo

--- testBashScript.sh
+++ testBashScript.sh
@@ -14,7 +14,7 @@ readCounts="workflow/out/readCounts"
 awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
 
 # now i attempt to do buildIndex???
-snakemake -p --snakefile snakefile_tester2.smk --cores 2 -forecall
+snakemake -p --snakefile snakefile_tester2.smk --cores 2 -forceall
 
 # this does rule removeGeneCatalogueDupicates:
 # awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
