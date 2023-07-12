0345d8c85869d3ad4fe0945a9109a418c7eda290
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-12 11:03:08 -0700

    Add files via upload

--- testBashScript.sh
+++ testBashScript.sh
@@ -10,28 +10,8 @@ sraRepo="workflow/out/scratch/prefetched"
 hitSummaries="workflow/out/bt_hit_summaries"
 readCounts="workflow/out/readCounts"
 
-# this does rule removeGeneCatalogueDupicates:
-awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
-
 # now i attempt to do buildIndex???
-
-
-
-
-
-# rule buildIndex:
-#    input:
-#        "workflow/out/gene_catalogues/{overall_pathway}_compiled_gene_catalogue_editIDs_noDups.fa"
-    # params:
-    #     index_name=lambda w: {w.overall_pathway}
-    # output:
-    #     join(config["indexDir"], "{overall_pathway}_gene_catalogue.1.bt2")
-    # shell:
-    #     """
-    #     bowtie2-build -f {input} workflow/out/index/{params.index_name}_gene_catalogue
-    #     """
-
-
-
-
 snakemake -p --snakefile snakefile_tester2.smk --cores 2
+
+# this does rule removeGeneCatalogueDupicates:
+# awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
