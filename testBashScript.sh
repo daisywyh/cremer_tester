7ddf917b3d8f999a513db0418f14f15bb8608e4c
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-13 17:24:59 +0000

    split out prefetch and dump from the main file

--- testBashScript.sh
+++ testBashScript.sh
@@ -10,11 +10,11 @@ sraRepo="workflow/out/scratch/prefetched"
 hitSummaries="workflow/out/bt_hit_summaries"
 readCounts="workflow/out/readCounts"
 
+# just do prefetch and dump
+# snakemake -p --snakefile workflow/rules/prefetchDump.smk --cores 2 --forceall
+
 # this does rule removeGeneCatalogueDupicates:
 awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
 
 # now i attempt to do buildIndex???
 snakemake -p --snakefile snakefile_tester2.smk --cores 2 --forceall
-
-# this does rule removeGeneCatalogueDupicates:
-# awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
