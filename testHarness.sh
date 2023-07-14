3e3e3a5b5d721e3dfbfbf1f089ca08c9724c037a
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-14 22:33:54 +0000

    added echo comments

--- testHarness.sh
+++ testHarness.sh
@@ -1,11 +1,11 @@
 set -euo pipefail
 
 # just do prefetch and dump
-#echo "doing prefetch + dump"
+echo "doing prefetch + dump"
 snakemake -p --snakefile workflow/rules/prefetchDump.smk --cores 5 --forceall
 
 # this does rule removeGeneCatalogueDupicates
-#echo "doing removeGeneCatalogueDuplicates"
+echo "doing removeGeneCatalogueDuplicates"
 awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs.fa" > "workflow/out/gene_catalogues/butyrate/butyrate_compiled_gene_catalogue_editIDs_noDups.fa"
 
 # now i attempt to do buildIndex???
