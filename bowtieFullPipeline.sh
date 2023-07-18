a1b97634bd326910d16dcea31e5f3dfc9dc2e7a0
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-18 18:12:34 +0000

    hard coded --forcerun

--- bowtieFullPipeline.sh
+++ bowtieFullPipeline.sh
@@ -13,22 +13,23 @@ echo "-=:___________/ "
 
 # read NUMCORES
 
+# default number for number of cores
 NUMCORES=5
 
-FORCERUN=true
+# FORCERUN=true
 
-RUNFLAG=""
+# RUNFLAG=""
 
-if [FORCERUN]
-then  
-   RUNFLAG="--forceall"
-fi 
-
+# # if user indicated they wanted to make this a forced run, change flags to indicate this
+# if [$FORCERUN]
+# then  
+#    RUNFLAG="--forceall"
+# fi 
 
 # just do prefetch and dump
 echo "doing prefetch + dump"
 echo "this might take a while ... (ಥ﹏ಥ)"
-snakemake --snakefile workflow/rules/prefetchDump.smk --cores $NUMCORES $RUNFLAG
+snakemake --snakefile workflow/rules/prefetchDump.smk --cores $NUMCORES --forcerun
 
 # this does rule removeGeneCatalogueDupicates
 echo "doing removeGeneCatalogueDuplicates"
@@ -38,16 +39,16 @@ awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_co
 echo "run runIndex.smk"
 echo "making the Bowtie index! (♡-_-♡)"
 echo "this might also take a while ... (ಥ﹏ಥ)"
-snakemake --snakefile workflow/rules/runIndex.smk --cores $NUMCORES $RUNFLAG
+snakemake --snakefile workflow/rules/runIndex.smk --cores $NUMCORES --forcerun
 
 echo "run runActualBowtie.smk ((ε(*´･ω･)っ†*ﾟ¨ﾟﾟ･*:..☆"
-snakemake --snakefile workflow/rules/runActualBowtie.smk --cores $NUMCORES $RUNFLAG
+snakemake --snakefile workflow/rules/runActualBowtie.smk --cores $NUMCORES --forcerun
 
 echo "run summarise.smk ᕕ(⌐■_■)ᕗ ♪♬"
-snakemake --snakefile workflow/rules/summarise.smk --cores $NUMCORES $RUNFLAG
+snakemake --snakefile workflow/rules/summarise.smk --cores $NUMCORES --forcerun
 
 echo "run finalCleanup.smk ｡+.｡☆ﾟ:;｡+ﾟ ☆*ﾟ¨ﾟﾟ･*:..ﾞ((ε(*⌒▽⌒)†"
-snakemake --snakefile workflow/rules/finalCleanup.smk --cores $NUMCORES $RUNFLAG
+snakemake --snakefile workflow/rules/finalCleanup.smk --cores $NUMCORES --forcerun
 
 echo "BOWTIE PIPELINE DONE! ＼＼\(۶•̀ᴗ•́)۶//／／"
 echo "୧(๑•̀ヮ•́)૭ LET'S GO!"
