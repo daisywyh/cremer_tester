494118f0884fb4d290bbfaafe52a85275ee02b34
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-18 17:26:27 +0000

    added variable for force run?

--- bowtieFullPipeline.sh
+++ bowtieFullPipeline.sh
@@ -15,10 +15,20 @@ echo "-=:___________/ "
 
 NUMCORES=5
 
+FORCERUN=true
+
+RUNFLAG=""
+
+if [FORCERUN]
+then  
+   RUNFLAG="--forceall"
+fi 
+
+
 # just do prefetch and dump
 echo "doing prefetch + dump"
 echo "this might take a while ... (ಥ﹏ಥ)"
-snakemake --snakefile workflow/rules/prefetchDump.smk --cores $NUMCORES --forceall & disown
+snakemake --snakefile workflow/rules/prefetchDump.smk --cores $NUMCORES $RUNFLAG
 
 # this does rule removeGeneCatalogueDupicates
 echo "doing removeGeneCatalogueDuplicates"
@@ -28,16 +38,16 @@ awk '/^>/{f=!d[$1];d[$1]=1}f' "workflow/out/gene_catalogues/butyrate/butyrate_co
 echo "run runIndex.smk"
 echo "making the Bowtie index! (♡-_-♡)"
 echo "this might also take a while ... (ಥ﹏ಥ)"
-snakemake --snakefile workflow/rules/runIndex.smk --cores $NUMCORES --forceall & disown
+snakemake --snakefile workflow/rules/runIndex.smk --cores $NUMCORES $RUNFLAG
 
 echo "run runActualBowtie.smk ((ε(*´･ω･)っ†*ﾟ¨ﾟﾟ･*:..☆"
-snakemake --snakefile workflow/rules/runActualBowtie.smk --cores $NUMCORES --forceall & disown
+snakemake --snakefile workflow/rules/runActualBowtie.smk --cores $NUMCORES $RUNFLAG
 
 echo "run summarise.smk ᕕ(⌐■_■)ᕗ ♪♬"
-snakemake --snakefile workflow/rules/summarise.smk --cores $NUMCORES --forceall & disown
+snakemake --snakefile workflow/rules/summarise.smk --cores $NUMCORES $RUNFLAG
 
 echo "run finalCleanup.smk ｡+.｡☆ﾟ:;｡+ﾟ ☆*ﾟ¨ﾟﾟ･*:..ﾞ((ε(*⌒▽⌒)†"
-snakemake --snakefile workflow/rules/finalCleanup.smk --cores $NUMCORES --forceall & disown
+snakemake --snakefile workflow/rules/finalCleanup.smk --cores $NUMCORES $RUNFLAG
 
 echo "BOWTIE PIPELINE DONE! ＼＼\(۶•̀ᴗ•́)۶//／／"
 echo "୧(๑•̀ヮ•́)૭ LET'S GO!"
