24701535726a3b9db10e689024f363bcb25a3202
Daisy Wang <59313915+daisywyh@users.noreply.github.com>
2023-07-20 18:30:43 +0000

    fixed typo

--- propionate_rerun/bowtieFullPipeline.sh
+++ propionate_rerun/bowtieFullPipeline.sh
@@ -33,7 +33,7 @@ snakemake --snakefile workflow/rules/prefetchDump.smk --cores $NUMCORES -p
 
 echo "doing editCatalogueIDs"
 echo "note: this step is run manually as a python command I hard coded"
-python3 workflow/scripts/edit_catalog_id.py "workflow/out/gene_catalogues/propionate_compiled_gene_catalogue.fa" "workflow/out/gene_catalogues/propionate_compiled_gene_catalogue_editIDs.fa"
+python3 workflow/scripts/edit_catalog_id.py "workflow/out/gene_catalogues/propionate/propionate_compiled_gene_catalogue.fa" "workflow/out/propionate/gene_catalogues/propionate_compiled_gene_catalogue_editIDs.fa"
 
 # this does rule removeGeneCatalogueDupicates
 echo "________________________________________________"
