61eb4eac301f525e517af5ec6130a6b3023df2fc
Daisy <wangyhdaisy@gmail.com>
2023-08-14 11:10:45 -0700

    push updates for pathway_analysis script

--- propionate_rerun/workflow/scripts/edit_catalog_id.py
+++ propionate_rerun/workflow/scripts/edit_catalog_id.py
@@ -19,3 +19,8 @@ for record in records:
 # print(new_records)
 
 SeqIO.write(new_records, new_file, "fasta")
+
+
+Alistipes-indistinctus-YIT-12060-MAF-2_methylmalonyl-CoA-decarboxylaseAlpha-KOK01604 1_2103/3-520 Alistipes-indistinctus-YIT-12060-MAF-2_methylmalonyl-CoA-decarboxylaseAlpha-KOK01604 1_2103;Alistipes-indistinctus-YIT-12060-MAF-2 1_2103 # 2575795 # 2577357 # -1 # ID=1_2103;partial=00;start_type=ATG;rbs_motif=None;rbs_spacer=None;gc_cont=0.575
+
+Anaerotruncus-colihominis-DSM-17241-MAF-2_buk 1_709/6-358 Anaerotruncus-colihominis-DSM-17241-MAF-2_buk 1_709;Anaerotruncus-colihominis-DSM-17241-MAF-2 1_709 # 745328 # 746407 # -1 # ID=1_709;partial=00;start_type=ATG;rbs_motif=AGGAGG;rbs_spacer=5-10bp;gc_cont=0.550
